dea996ea87e4fc3f53482a0c452d7601bb3395a6
raywritescode <raywritescode@users.noreply.github.com>
2014-09-29 15:00:45 -0700

    README.md - Update mode.

---
+++ ch02/examples/Welcome1.java
@@ -0,0 +1,15 @@
+// Fig. 2.1: Welcome1.java
+// Text-printing program
+// Page 38
+//
+// Ray Santos
+// September 29, 2014
+//
+
+public class Welcome1
+{
+   public static void main( String[] args )
+   {
+      System.out.println( "Welcome to Java Programming!" );
+   }
+}
