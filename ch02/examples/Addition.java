ecd144d9a5a1506743ee3f9ab4d533b7d7b87fcc
raywritescode <raywritescode@users.noreply.github.com>
2014-09-29 18:23:26 -0700

    Addition.java - Added

---
+++ ch02/examples/Addition.java
@@ -0,0 +1,31 @@
+// Fig. 2.7: Addition.java
+// Addition program that displays the sum of two numbers.
+// Page 47
+//
+// Ray Santos
+// September 29, 2014
+
+import java.util.Scanner; // program uses class Scanner
+
+public class Addition
+{
+   public static void main( String[] args )
+   {
+      // create a Scanner to obtain input from the command window
+      Scanner input = new Scanner( System.in );
+
+      int number1;
+      int number2;
+      int sum;
+
+      System.out.print( "Enter first integer: " );
+      number1 = input.nextInt();
+
+      System.out.print( "Enter second integer: " );
+      number2 = input.nextInt();
+
+      sum = number1 + number2;
+
+      System.out.printf( "Sum is %d\n\n", sum );
+   }
+}
