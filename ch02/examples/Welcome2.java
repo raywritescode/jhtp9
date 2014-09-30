b8dfdeb0cf144bfd89e24cb957877ca283c3f43c
raywritescode <raywritescode@users.noreply.github.com>
2014-09-29 18:07:34 -0700

    Welcome2.java - Added

---
+++ ch02/examples/Welcome2.java
@@ -0,0 +1,16 @@
+// Fig. 2.3: Welcome2.java
+// Printing aline of text with multiple statements
+// Page 44
+//
+// Ray Santos
+// September 29, 2014
+//
+
+public class Welcome2
+{
+   public static void main( String[] args )
+   {
+      System.out.print( "Welcome to " );
+      System.out.println( "Java Programming!" );
+   }
+}
