f9de535eb730b818e28c881ac5536ade0308f317
raywritescode <raywritescode@users.noreply.github.com>
2014-09-29 18:11:43 -0700

    Welcome3.java - Added

---
+++ ch02/examples/Welcome3.java
@@ -0,0 +1,15 @@
+// Fig. 2.4: Welcome3.java
+// Printing multiple lines of text with a single statement.
+// Page 45
+//
+// Ray Santos
+// September 29, 2014
+//
+
+public class Welcome3
+{
+   public static void main( String[] args )
+   {
+      System.out.println( "Welcome\nto\nJava\nProgramming!" );
+   }
+}
