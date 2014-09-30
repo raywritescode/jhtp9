c522b3e608d27b8157a548a85f88a64519fef000
raywritescode <raywritescode@users.noreply.github.com>
2014-09-29 18:17:11 -0700

    Welcome4.java - Added

---
+++ ch02/examples/Welcome4.java
@@ -0,0 +1,16 @@
+// Fig. 2.6: Welcome4.java
+// Displaying multiple lines with method System.out.printf
+// Page 46
+//
+// Ray Santos
+// September 29, 2014
+//
+
+public class Welcome4
+{
+   public static void main( String[] args )
+   {
+      System.out.printf( "%s\n%s\n",
+         "Welcome to", "Java Programming!" );
+   }
+}
