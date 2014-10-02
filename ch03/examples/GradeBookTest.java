e94bfe711491c9242df79f4a114ee160d7941394
raywritescode <raywritescode@users.noreply.github.com>
2014-10-01 17:58:29 -0700

    GradeBookTest.java - Added

---
+++ ch03/examples/GradeBookTest.java
@@ -0,0 +1,16 @@
+// Fig. 3.2: GradeBookTest.java
+// Creating a GradeBook object and calling its displayMessage method.
+// Page 74
+//
+// Ray Santos
+// October 1, 2014
+
+public class GradeBookTest
+{
+   public static void main( String[] args )
+   {
+      GradeBook myGradeBook = new GradeBook();
+
+      myGradeBook.displayMessage();
+   }
+}
