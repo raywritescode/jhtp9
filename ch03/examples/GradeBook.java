56578f334dc45b567d69d15a81ba6a777495abf3
raywritescode <raywritescode@users.noreply.github.com>
2014-10-01 17:57:51 -0700

    GradeBook.java - Added

---
+++ ch03/examples/GradeBook.java
@@ -0,0 +1,14 @@
+// Fig. 3.1: GradeBook.java
+// Class declaration with one method
+// Page 73
+//
+// Ray Santos
+// October 1, 2014
+
+public class GradeBook
+{
+   public void displayMessage()
+   {
+      System.out.println("Welcome to the Grade Book!");
+   }
+}
