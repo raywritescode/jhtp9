40f2e0428b71c0ccfbddf817177190a017303693
raywritescode <raywritescode@users.noreply.github.com>
2014-10-02 10:39:32 -0700

    Dialog1.java - Added

---
+++ ch03/examples/Dialog1.java
@@ -0,0 +1,16 @@
+// Fig. 3.17: Dialog1.java
+// Using JOptionPane to display multiple lines in a dialog box.
+// Java How to Program, 9th Ed. (Deitel) Page 93
+//
+// Ray Santos
+// October 2, 2014
+
+import javax.swing.JOptionPane;
+
+public class Dialog1
+{
+   public static void main( String[] args )
+   {
+      JOptionPane.showMessageDialog( null, "Welcome\nto\nJava" );
+   }
+}
