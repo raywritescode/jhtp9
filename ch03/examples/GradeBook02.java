d2e62f51ee6ca8bb2d36c343b92d58b3c18cd204
raywritescode <raywritescode@users.noreply.github.com>
2014-10-01 18:10:13 -0700

    GradeBook02.java - Added

---
+++ ch03/examples/GradeBook02.java
@@ -0,0 +1,15 @@
+// Fig. 3.4: GradeBook02.java
+// Class declaration with one method that has a parameter
+// Page 77
+//
+// Ray Santos
+// October 1, 2014
+
+public class GradeBook02
+{
+   public void displayMessage( String courseName )
+   {
+      System.out.printf("Welcome to the grade book for\n%s!\n",
+         courseName);
+   }
+}
