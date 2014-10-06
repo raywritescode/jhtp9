bec06bd3e2155d5de0ff14d6cbd7308c8e201bdc
raywritescode <raywritescode@users.noreply.github.com>
2014-10-05 22:18:05 -0700

    GradeBook01.java - Added

---
+++ ch04/examples/GradeBook01.java
@@ -0,0 +1,58 @@
+// Fig. 4.6: GradeBook01.java
+// GradeBook01 class that solves class-average problem
+// using counter-controlled repetition.
+// 
+// Java How To Program, 9th Ed. (Deitel) Page 114
+//
+// Ray Santos
+// October 5, 2014
+
+import java.util.Scanner;
+
+public class GradeBook01
+{
+   private String courseName;
+
+   // constructor
+   public GradeBook01( String name )
+   {
+      courseName = name;
+   }
+
+   public String getCourseName()
+   {
+      return courseName;
+   }
+
+   public void displayMessage()
+   {
+      System.out.printf( "Welcome to the grade book for\n%s!\n\n\",
+         getCourseName() );
+   }
+
+   public void determineClassAverage()
+   {
+      Scanner input = new Scanner( System.in );
+
+      int total;
+      int gradeCounter;
+      int grade;
+      int average;
+
+      total = 0;
+      gradeCounter = 1;
+
+      while ( gradeCounter <= 10 )
+      {
+         System.out.print( "Enter grade: ");
+         grade = input.nextInt();
+         total = total + grade;
+         gradeCounter = gradeCounter + 1;
+      }
+
+      average = total / 10;
+
+      System.out.printf( "\nTotal of all 10 grades is %d\n", total );
+      System.out.printf( "Class average is %d\n\n", average );
+   }
+}
