e7824d8e03d4e20867ff26d1d07240cfa61f3b84
raywritescode <raywritescode@users.noreply.github.com>
2014-10-05 22:40:26 -0700

    GradeBook01Test.java - Updated to include a sample run of the program

--- ch04/examples/GradeBook01Test.java
+++ ch04/examples/GradeBook01Test.java
@@ -17,3 +17,24 @@ public class GradeBook01Test
       myGradeBook.determineClassAverage();
    }
 }
+
+/* Example run
+
+Welcome to the grade book for
+CS101 Introduction to Java Programming!
+
+Enter grade: 67
+Enter grade: 78
+Enter grade: 89
+Enter grade: 67
+Enter grade: 87
+Enter grade: 98
+Enter grade: 93
+Enter grade: 85
+Enter grade: 82
+Enter grade: 100
+
+Total of all 10 grades is 846
+Class average is 84
+
+*/
