89880bebaff1a64f27f1b3670d7b84a7e2418848
raywritescode <raywritescode@users.noreply.github.com>
2014-10-06 18:45:27 -0700

    DrawPanel.java - Added

---
+++ ch04/examples/DrawPanel.java
@@ -0,0 +1,29 @@
+// Fig. 4.18: DrawPanel.java
+// Using drawLine to connect the corners of a panel.
+//
+// Java How To Program, 9th Ed. (Deitel) Page 135
+//
+// Ray Santos
+// October 6, 2014
+
+import java.awt.Graphics;
+import java.swing.JPanel;
+
+public class DrawPanel extends JPanel
+{
+   // draws an X from the corners of the panel
+   public void paintComponent( Graphics g )
+   {
+      // call paintComponent to ensure the panel displays correctly
+      super.paintComponent( g );
+
+      int width = getWidth();
+      int height = getHeight();
+
+      // draw a line from the upper-left to the lower-right
+      g.drawLine( 0, 0, width, height );
+
+      // draw a line from the lower-left to the upper-right
+      g.drawLine( 0, height, width, 0 );
+   }
+}
