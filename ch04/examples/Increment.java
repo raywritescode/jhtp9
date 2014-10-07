82aa4de15b7bd3a88fe690e58377ccefe91b7485
raywritescode <raywritescode@users.noreply.github.com>
2014-10-06 18:27:26 -0700

    Increment.java - Added

---
+++ ch04/examples/Increment.java
@@ -0,0 +1,40 @@
+// Fig. 4.15: Increment.java
+// Prefix increment and postfix increment operators
+//
+// Java How To Program, 9th Ed. (Deitel) Page 132
+//
+// Ray Santos
+// October 6, 2014
+
+public class Increment
+{
+   public static void main( String[] args )
+   {
+      int c;
+
+      // demonstrate postfix increment operator
+      c = 5;
+      System.out.println( c );   // prints 5
+      System.out.println( c++ ); // prints 5 then postincrements
+      System.out.println( c );   // prints 6
+
+      System.out.println();
+
+      c = 5;
+      System.out.println( c );   // prints 5
+      System.out.println( ++c ); // preincrements then prints 6
+      System.out.println( c );   // prints 6
+   }
+}
+
+/* Test run
+
+5
+5
+6
+
+5
+6
+6
+
+*/
