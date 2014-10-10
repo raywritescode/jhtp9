b8fc81185a54ebf92fe82fa67c135ed544f333c6
raywritescode <raywritescode@users.noreply.github.com>
2014-10-09 20:54:13 -0700

    ForCounter.java - Added

---
+++ ch05/examples/ForCounter.java
@@ -0,0 +1,24 @@
+// Fig. 5.2: ForCounter.java
+// Counter-controlled repetition with the for repetition statement.
+//
+// Java How to Program, 9th Ed. (Deitel) Page 154
+//
+// Ray Santos
+// October 9, 2014
+
+public class ForCounter
+{
+   public static void main( String[] args )
+   {
+      for ( int counter = 1; counter <= 10; counter++ )
+         System.out.printf( "%d  ", counter );
+
+      System.out.println();
+   }
+}
+
+/* Code execution
+
+1  2  3  4  5  6  7  8  9  10
+
+*/
