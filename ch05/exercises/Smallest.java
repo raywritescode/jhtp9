cef09aac11d434c7bcae3c713292f79de0048375
raywritescode <raywritescode@users.noreply.github.com>
2014-10-15 18:36:27 -0700

    Smallest.java - Added Java code for the pseudocode algorithm

--- ch05/exercises/Smallest.java
+++ ch05/exercises/Smallest.java
@@ -10,25 +10,44 @@
 // Ray Santos
 // October 15, 2014
 
-/* Pseudocode
+import java.util.Scanner;
 
-Initialize values to zero (as integer)
-Initialize number to zero (as integer)
-Initialize smallest to zero (as integer)
+public class Smallest
+{
+   private int values; // Initialize values to zero (as integer)
+   private int number; // Initialize number to zero (as integer)
+   private int smallest; // Initialize smallest to zero (as integer)
 
-Prompt the user for number of values
-Input the values
+   public void inputValues()
+   {
+      // Prompt the user for number of values
+      System.out.print( "Enter the number of integer values to compare: " );
 
-For the number of values
-   Prompt the user for a number
-   Input the number
+      // Input the values
+      Scanner input = new Scanner( System.in );
+      values = input.nextInt();
 
-   If the number is the first input
-      assign number to smaller
-   else
-      If the number is less than smaller
-         assign number to smaller
+      // For the number of values
+      for ( int i = 1; i <= values; i++ )
+      {
+         // Prompt the user for a number
+         System.out.printf( "Enter an integer for value %d: ", i ); 
+         // Input the number
+         number = input.nextInt();
 
-Print the smallest number
+         // If the number is the first input
+         if ( i == 1 )
+            smallest = number; // assign number to smaller
+         else
+            // If the number is less than smaller
+            if ( number < smallest )
+               smallest = number; // assign number to smaller
+      }
+   }
 
-*/
+   // Print the smallest number
+   public void displaySmallest()
+   {
+      System.out.printf( "\nThe smallest integer is %d\n\n", smallest );
+   }
+}
