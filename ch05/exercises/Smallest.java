7c65e9a652ca398587ada8db7e046e97d1d8bd71
raywritescode <raywritescode@users.noreply.github.com>
2014-10-15 18:12:14 -0700

    Smallest.java - Added. Pseudocode

---
+++ ch05/exercises/Smallest.java
@@ -0,0 +1,34 @@
+// Exercise 5.11: Smallest.java
+// Find the smallest value
+//
+// Write an application that finds the smallest of several integers.
+// Assume that the first value read specifies the number of values
+// to input from the user.
+//
+// Java How to Program, 9th Ed. (Deitel) Page 193
+//
+// Ray Santos
+// October 15, 2014
+
+/* Pseudocode
+
+Initialize values to zero (as integer)
+Initialize number to zero (as integer)
+Initialize smallest to zero (as integer)
+
+Prompt the user for number of values
+Input the values
+
+For the number of values
+   Prompt the user for a number
+   Input the number
+
+   If the number is the first input
+      assign number to smaller
+   else
+      If the number is less than smaller
+         assign number to smaller
+
+Print the smallest number
+
+*/
