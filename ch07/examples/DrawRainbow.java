bb4434fea8fd246764367b35ebf8db9492f6883a
raywritescode <raywritescode@users.noreply.github.com>
2014-11-05 17:04:01 -0800

    DrawRainbow.java - Added

---
+++ ch07/examples/DrawRainbow.java
@@ -0,0 +1,52 @@
+// Fig. 7.25: DrawRainbow.java
+// Demonstrates using colors in an array.
+//
+// Java How to Program, 9th Ed. (Deitel) Page 287
+//
+// Ray Santos
+// November 5, 2014
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawRainbow
+{
+   private final static Color VIOLET = new Color( 128, 0, 128 );
+   private final static Color INDIGO = new Color( 75, 0, 130 );
+
+   // colors to us in the rainbow, starting from the innermost.
+   // The two white entries result in an empty arc in the center.
+   private Color[] colors =
+      { Color.WHITE, Color.WHITE, VIOLET, INDOGO, Color.BLUE,
+        Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED };
+
+   public DrawRainbow()
+   {
+      setBackground( Color.WHITE );
+   }
+
+   // draws rainbow using concentric arcs
+   public void paintComponents( Graphics g )
+   {
+      super.paintComponent( g );
+
+      int radius = 20; arc radius
+
+      // draw the rainbow near the bottom-center
+      int centerX = getWidth() / 2;
+      int centerY = getHeight() - 10;
+
+      // draw filled arcs starting with the outermost
+      for ( int counter = colors.length; counter > 0; counter-- )
+      {
+         // set the color for the current arc
+         g.setColor( colors[ counter - 1 ] );
+
+         // fill the arc from 0 to 180 degrees
+         g.fillArc( centerX - counter * radius,
+            centerY - counter * radius,
+            counter * radius * 2, counter * radius * 2, 0, 180 );
+      }
+   }
+}
