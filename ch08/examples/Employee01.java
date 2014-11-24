4961e9616bf8a748dacc46889fa9cad23df70d79
raywritescode <raywritescode@users.noreply.github.com>
2014-11-23 19:24:13 -0800

    Employee01.java - Added

---
+++ ch08/examples/Employee01.java
@@ -0,0 +1,40 @@
+// Fig. 8.12: Employee01.java
+// Static variable used to maintain a count of the number
+// of Employee objects in memory.
+//
+// Java How to Program, 9th Ed. (Deitel) Page 336
+//
+// Ray Santos
+// November 23, 2014
+
+public class Employee01
+{
+   private String firstName;
+   private String lastName;
+   private static int count = 0;
+
+   public Employee01( String first, String last )
+   {
+      firstName = first;
+      lastName = last;
+
+      ++count;
+      System.out.printf( "Employee constructor: %s %s; count = %d\n",
+         firstName, lastName, count );
+   }
+
+   public String getFirstName()
+   {
+      return firstName;
+   }
+
+   public String getLastName()
+   {
+      return lastName;
+   }
+
+   public static int getCount()
+   {
+      return count;
+   }
+}
