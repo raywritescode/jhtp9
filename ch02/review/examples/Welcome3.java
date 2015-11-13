7a4ce5e200318d4daece262ea81f48c23ac30ba4
raywritescode <raywritescode@users.noreply.github.com>
2015-11-12 20:23:36 -0800

    Added - Welcome3.java

---
+++ ch02/review/examples/Welcome3.java
@@ -0,0 +1,12 @@
+// Welcome3.java
+// November 12, 2015
+// JHTP9, page 45
+// Ray Santos
+
+public class Welcome3
+{
+   public static void main( String[] args )
+   {
+      System.out.println( "Welcome\nto\nJava\nProgramming\nReview!" );
+   }
+}
