748cd47537481b378f393191520dbd5cd37b1de0
raywritescode <raywritescode@users.noreply.github.com>
2015-11-12 20:20:55 -0800

    Added - Welcome2.java

---
+++ ch02/review/examples/Welcome2.java
@@ -0,0 +1,13 @@
+// Welcome2.java
+// November 12, 2015
+// JHTP9, page 44
+// Ray Santos
+
+public class Welcome2
+{
+   public static void main( String[] args )
+   {
+      System.out.print( "Welcome to " );
+      System.out.println( "Java Programming review!" );
+   }
+}
