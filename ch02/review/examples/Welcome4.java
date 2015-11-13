53e3b3c6264dc71cb847016a5fa2840298f22fb2
raywritescode <raywritescode@users.noreply.github.com>
2015-11-12 20:26:13 -0800

    Added - Welcome4.java

---
+++ ch02/review/examples/Welcome4.java
@@ -0,0 +1,12 @@
+// Welcome4.java
+// November 12, 2015
+// JHTP9, page 46
+// Ray Santos
+
+public class Welcome4
+{
+   public static void main( String[] args )
+   {
+      System.out.printf( "%s\n%s\n", "Welcome to", "Java Programming review!" );
+   }
+}
